5a6a3888b11448d62367d6011c0135703062fc04
jengzang <yangzheng031209@qq.com>
2026-08-08 14:14:44 +0800

    refactor: 抽取 usePhosphorMap 共享 composable，新增 InlineIcon 行内图标组件

--- project/src/components/common/InlineIcon.vue
+++ project/src/components/common/InlineIcon.vue
@@ -2,13 +2,13 @@
   <component
     v-if="phosphorIcon"
     :is="phosphorIcon"
-    class="bar-icon-svg"
+    class="inline-icon-svg"
     :size="'1em'"
-    :weight="phosphorWeight"
+    :weight="'regular'"
     :mirrored="false"
     color="currentColor"
   />
-  <span v-else class="bar-icon-emoji">{{ icon }}</span>
+  <span v-else class="inline-icon-emoji">{{ icon }}</span>
 </template>
 
 <script setup>
@@ -16,11 +16,16 @@ import { computed } from 'vue'
 import { resolvePhosphorIcon } from '@/composables/bar/usePhosphorMap.js'
 
 const props = defineProps({
-  icon: { type: String, required: true },
-  weight: { type: String, default: 'bold' }
+  icon: { type: String, required: true }
 })
-
-const phosphorWeight = computed(() => props.weight)
 
 const phosphorIcon = computed(() => resolvePhosphorIcon(props.icon))
 </script>
+
+<style scoped>
+.inline-icon-svg,
+.inline-icon-emoji {
+  display: inline;
+  vertical-align: -0.125em;
+}
+</style>
