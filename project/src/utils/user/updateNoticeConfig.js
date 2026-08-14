9e16b08d31fc494114758bfa1035ff3288d20e68
jengzang <yangzheng031209@qq.com>
2026-08-15 03:41:15 +0800

    update:更新日志

--- project/src/utils/user/updateNoticeConfig.js
+++ project/src/utils/user/updateNoticeConfig.js
@@ -1,14 +1,14 @@
 export function getHomeUpdateNotice(t) {
     const items = [
         {
-            icon: '🎲',
-            strong: '教程完善',
-            text: '各個頁面均配備教程指引用戶操作，並且教程簡單校對過一遍了，不再是毫無價值的AI文章了。'
+            icon: '👀',
+            strong: '音節統計、熱力圖、等值線圖',
+            text: '音節統計增加完整音節統計，並可繪製音節總數等值線圖，地圖-分區圖增加熱力圖，地點詳情彈窗可以直接跳轉查看音系、演化、音節統計等。'
         },
         {
-            icon: '👀',
-            strong: '優化用戶體驗(首頁、導航欄)',
-            text: '首頁增加地球和燈箱展示網站圖片，導航欄圖標與動畫優化，字體優化'
+            icon: '🎲',
+            strong: '教程完善與優化用戶體驗',
+            text: '各個頁面均配備教程指引用戶操作，並且教程簡單校對過一遍了，不再是毫無價值的AI文章了。'
         },
         {
             icon: '📖',
@@ -17,9 +17,9 @@ export function getHomeUpdateNotice(t) {
         },
     ]
   return {
-    version: 'v5.0.8',
+    version: 'v5.1.0',
     dbVersion: '2026-06-22',
-    lastUpdateDate: '2026-08-09',
+    lastUpdateDate: '2026-08-13',
     title: t('home.updateNotice.title'),
     items
   }
